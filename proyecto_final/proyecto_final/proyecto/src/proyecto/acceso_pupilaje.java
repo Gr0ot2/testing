6b6f5a934aa49abfbec5ebc9f777592e5c597da3
Gr0ot2 <105909506+Gr0ot2@users.noreply.github.com>
2023-05-30 09:14:02 +0200

    Update acceso_pupilaje.java

--- proyecto_final/proyecto_final/proyecto/src/proyecto/acceso_pupilaje.java
+++ proyecto_final/proyecto_final/proyecto/src/proyecto/acceso_pupilaje.java
@@ -2,6 +2,7 @@ package proyecto;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -17,123 +18,159 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.JOptionPane;
 
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+import java.sql.Statement;
 
 /**
  * Servlet implementation class acceso_pupilaje
  */
 @WebServlet("/acceso_pupilaje")
 public class acceso_pupilaje extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public acceso_pupilaje() {
-        super();
-        // TODO Auto-generated constructor stub
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public acceso_pupilaje() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  Connection conn = null;
+  String user = "root";
+  String driver = "com.mysql.cj.jdbc.Driver";
+  String pwd = "";
+  String url = "jdbc:mysql://localhost:3306/caballos?serverTimezone=UTC";
+
+  public void init(ServletConfig config) throws ServletException {
+    try {
+      Class.forName(driver);
+      conn = DriverManager.getConnection(url, user, pwd);
+
+      JOptionPane.showMessageDialog(null, "hemos conectado");
+
+    } catch (ClassNotFoundException e1) {
+      //Error si no puedo leer el driver de Oracle 
+      JOptionPane.showMessageDialog(null, "ERROR:No encuentro el driver de la BD: " +
+        e1.getMessage());
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     }
+  }
 
-    Connection conn=null;
-  	String user="root";
-  	String driver="com.mysql.cj.jdbc.Driver";
-  	String pwd="";
-  	String url="jdbc:mysql://localhost:3306/caballos?serverTimezone=UTC";
-  	
+  protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String user;
+    String password;
+    String obtenerPassword = null;
+    String obtenerUsuario = null;
 
+    PreparedStatement insertarContrase√±a;
+    PreparedStatement eliminarContrasena;
+    ResultSet obtenerUser = null;
+    ResultSet comprobarUser = null;
 
-      public void init(ServletConfig config) throws ServletException{
-  		try {
-  		    Class.forName(driver);
-  			conn=DriverManager.getConnection(url,user,pwd);
-
-  			JOptionPane.showMessageDialog(null,"hemos conectado");
-
-  		} 
-  		catch (ClassNotFoundException e1) {
-  		            //Error si no puedo leer el driver de Oracle 
-  			JOptionPane.showMessageDialog(null, "ERROR:No encuentro el driver de la BD: "+
-  					e1.getMessage());
-  		} catch (SQLException e) {
-  			// TODO Auto-generated catch block
-  			e.printStackTrace();
-  		}
+    PrintWriter out;
+    out = response.getWriter();
+    user = request.getParameter("user");
+    if (request.getParameter("eliminar") != null) {
+      if (user.length() == 0) {
+        JOptionPane.showMessageDialog(null, "por favor inserte el nombre de su usuario");
+        response.sendRedirect("acceso_pupilaje.jsp");
+      } else {
+        try {
+          int fecha = Integer.parseInt(JOptionPane.showInputDialog("cuando naci√≥ tu caballo"));
+          if (fecha == 2) {
+            eliminarContrasena = conn.prepareStatement(
+              "update `pupilaje` set password = 0 where user='" + user + "';");
+            eliminarContrasena.executeUpdate();
+            JOptionPane.showMessageDialog(null, "contrase√±a eliminada por favor inserte su nueva contrase√±a");
+            response.sendRedirect("acceso_pupilaje.jsp");
+          } else {
+            JOptionPane.showMessageDialog(null, "lo siento no es correcto");
+            response.sendRedirect("pagina_principal.jsp");
+          }
+        } catch (SQLException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+          System.out.println(e);
+        }
       }
-
-      protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-    	  String user;
-    	  String password;
-    	  String obtenerPassword=null;
-    	  String obtenerUsuario=null;
-
-        	PreparedStatement insertarContraseÒa;
-      	ResultSet obtenerUser=null;
-      	ResultSet comprobarUser=null;
-
-
-    	  
-    	  PrintWriter out;
-  		out=response.getWriter();		
-  		user=request.getParameter("user");
-  		password=request.getParameter("password");
-  		
-  		try {
-  			out.println("<html>");
-    		out.println("<head><title>service</title></head>");
-    		out.println("<body>");
-			java.sql.Statement smt=conn.createStatement();
-			
-			String seleccionUser=
-					"select password from `pupilaje` where user='"+user+"';";
-			obtenerUser=smt.executeQuery(seleccionUser);
-			while(obtenerUser.next()) {
-				 obtenerPassword= obtenerUser.getString(1);
-				if(obtenerPassword.length()==0) {
-				insertarContraseÒa=conn.prepareStatement(
-						"update `pupilaje` set password='"+password+"' where user='"+user+"';");
-				insertarContraseÒa.executeUpdate();
-				}			
-				 if(obtenerPassword.length()!=0){
-					 if(!obtenerPassword.equals(password)){
-					JOptionPane.showMessageDialog(null, "lo siento no ha escrito bien su contraseÒa");
-					RequestDispatcher rd=
-							request.getRequestDispatcher("acceso_pupilaje.jsp");
-					rd.forward(request, response);
-					 }
-				}	 
-				 }
-			 String obtainUser=
-					 "select user from `pupilaje` where user='"+user+"';";
-			 comprobarUser=smt.executeQuery(obtainUser);
-			 while(comprobarUser.next()) {
-				 nombreC c= new nombreC();
-				 obtenerUsuario= comprobarUser.getString(1);
-				 if(obtenerUsuario.length()!=0){
-				 if(obtenerUsuario.equals("Esther")) {
-					 response.sendRedirect("Seleccion_esther.jsp");
-					 String nombre=c.setNombre("caballos");
-					 c.getNombre(nombre);
-				 }
-				 else if(obtenerUsuario.equals("Marinera")) {
-					 response.sendRedirect("relampago.jsp");
-					 String nombre=c.setNombre("relampago");
-					 c.getNombre(nombre);
-				 }
-				 else if(obtenerUsuario.equals("hectorino")) {
-					 response.sendRedirect("caballo_seleccion.jsp");
-				 }
-				 else if(!obtenerUsuario.equals("hectorino")||!obtenerUsuario.equals("Marinera")||!obtenerUsuario.equals("Esther")) {
-					 JOptionPane.showMessageDialog(null, "no est· registrado en la base de datos ");
-				 }
-				 }
-			 }
-			
-			
-				}
-  				catch (Exception e) {
-  		  			System.out.println(e);
-			// TODO: handle exception
-		}
-  		
+    } else {
+      try {
+        password = request.getParameter("password");
+        JOptionPane.showMessageDialog(null, password);
+        byte[] claveEnBytes = password.getBytes("UTF-8");
+        SecretKeySpec secretKey = new SecretKeySpec(claveEnBytes, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] passwordEncriptada = cipher.doFinal(claveEnBytes);
+        String passwordEncriptadaBase64 = Base64.getEncoder().encodeToString(passwordEncriptada);
+        JOptionPane.showMessageDialog(null, "3" + passwordEncriptadaBase64);
+        out.println("<html>");
+        out.println("<head><title>service</title></head>");
+        out.println("<body>");
+        Statement smt = conn.createStatement();
+        String seleccionUser =
+          "select password from `pupilaje` where user='" + user + "';";
+        obtenerUser = smt.executeQuery(seleccionUser);
+        while (obtenerUser.next()) {
+          obtenerPassword = obtenerUser.getString(1);
+          if (obtenerPassword.length() == 0 || obtenerPassword.equals("0")) {
+            insertarContrase√±a = conn.prepareStatement(
+              "update `pupilaje` set password='" + passwordEncriptadaBase64 + "' where user='" + user + "';");
+            insertarContrase√±a.executeUpdate();
+          }
+          if (obtenerPassword.length() != 0 && !obtenerPassword.equals("0")) {
+            if (!obtenerPassword.equals(passwordEncriptadaBase64)) {
+              JOptionPane.showMessageDialog(null, "lo siento no ha escrito bien su contrase√±a");
+              RequestDispatcher rd =
+                request.getRequestDispatcher("acceso_pupilaje.jsp");
+              rd.forward(request, response);
+            }
+          }
+        }
+        String obtainUser =
+          "select user from `pupilaje` where user='" + user + "';";
+        comprobarUser = smt.executeQuery(obtainUser);
+        while (comprobarUser.next()) {
+          nombreC c = new nombreC();
+          obtenerUsuario = comprobarUser.getString(1);
+          if (obtenerUsuario.length() != 0) {
+            if (obtenerUsuario.equals("Esther")) {
+              response.sendRedirect("Seleccion_esther.jsp");
+              String nombre = c.setNombre("caballos");
+              c.getNombre(nombre);
+            } else if (obtenerUsuario.equals("Marina")) {
+              response.sendRedirect("relampago.jsp");
+              String nombre = c.setNombre("relampago");
+              c.getNombre(nombre);
+            } else if (obtenerUsuario.equals("Hector")) {
+              response.sendRedirect("caballo_seleccion.jsp");
+            } else if (!obtenerUsuario.equals("Hector") || !obtenerUsuario.equals("Marina") || !obtenerUsuario.equals("Esther")) {
+              JOptionPane.showMessageDialog(null, "no est√° registrado en la base de datos ");
+              response.sendRedirect("acceso_pupilaje.jsp");
+            }
+          }
+        }
+      } catch (NoSuchAlgorithmException e1) {
+        // TODO Auto-generated catch block
+        e1.printStackTrace();
+        System.out.println(e1);
+        response.sendRedirect("acceso_pupilaje.jsp");
+      } catch (NoSuchPaddingException e1) {
+        // TODO Auto-generated catch block
+        e1.printStackTrace();
+        System.out.println(e1);
+        response.sendRedirect("acceso_pupilaje.jsp");
+      } catch (Exception e) {
+        System.out.println(e);
+        response.sendRedirect("acceso_pupilaje.jsp");
+        // TODO: handle exception
       }
-
+    }
+  }
 }
